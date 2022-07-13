00e2971b0d9f23a6a9bf7e53ac0c3147e62091e3
Jesus Alberto Castillo <jesusach92@gmail.com>
2022-07-13 14:03:01 -0500

    Avance

--- src/Config/database.js
+++ src/Config/database.js
@@ -1,17 +1,9 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import mysql from "mysql2/promise";
-
-dotenv.config()
-
-const config = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-};
-
-
-
-export const connect = async () => {
-    return await mysql.createConnection(config);
-}
+dotenv.config();
+const config = {  
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,};
+export const connect = async () => {return await mysql.createConnection(config);};
